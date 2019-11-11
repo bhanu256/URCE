d2149952568bdef4bba8101e3b588d46ef198404
Bhanu Chowdary <bhanuphnai100@gmail.com>
2019-11-11 16:59:17 +0530

    Integrating home page to project - glide added and retrival of photos from firebase

--- app/src/main/java/com/specialteam/urce/Home.java
+++ app/src/main/java/com/specialteam/urce/Home.java
@@ -4,26 +4,38 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+
+import com.bumptech.glide.Glide;
+import com.google.firebase.database.Query;
 
 public class Home extends AppCompatActivity{
 
     Intent intent;
     Bundle bd;
+
+    ContextAdaptor adaptor;
 
     private DatabaseReference databaseReference;
     FirebaseAuth auth;
@@ -84,6 +96,36 @@ public class Home extends AppCompatActivity{
                 return false;
             }
         });
+
+        recycleload();
     }
 
+    public void recycleload(){
+        Query query = FirebaseDatabase.getInstance().getReference().child("Container");
+
+        FirebaseRecyclerOptions<DataFromAdaptor> options = new FirebaseRecyclerOptions.Builder<DataFromAdaptor>()
+                .setQuery(query,DataFromAdaptor.class)
+                .build();
+
+        adaptor = new ContextAdaptor(getApplicationContext(),options);
+
+        RecyclerView recyclerView = findViewById(R.id.recycle);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adaptor);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adaptor.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adaptor.stopListening();
+    }
 }
+
+
